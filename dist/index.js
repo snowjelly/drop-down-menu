d48ef46aab40408866e4cf423a1e2cc1cf4faf37
Connor Broadwell <broadwellconnor@gmail.com>
2023-04-26 11:17:01 -0500

    added another nav, refactor code

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,34 @@
 function openNav(elId) {
   const nav = document.getElementById(elId);
 
-  nav.classList.toggle('visible');
+  nav.setAttribute('open', '');
+}
+
+function closeNav(elId) {
+  const nav = document.getElementById(elId);
+
+  nav.removeAttribute('open');
+}
+
+function toggleNav(elId) {
+  const nav = document.getElementById(elId);
+
+  if (nav.getAttribute('open') !== null) {
+    closeNav(elId);
+  }
+  else if (nav.getAttribute('open') === null) {
+    openNav(elId);
+  }
 }
 
 
 const openNavButton = document.getElementById('button-open-nav');
 openNavButton.addEventListener('click', () => {
-  openNav('nav');
+  toggleNav('nav');
+});
+
+const openInteriorNavButton = document.getElementById('button-open-add-task');
+openInteriorNavButton.addEventListener('click', (e) => {
+  toggleNav('add-task');
+  e.currentTarget.classList.toggle('rotate');
 });
